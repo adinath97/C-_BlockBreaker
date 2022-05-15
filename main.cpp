e7e482ae70dd9b1708124f68aad91ac79331145c
adinath97 <58251104+adinath97@users.noreply.github.com>
2022-05-15 00:11:53 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <SDL.h>
+#include <stdio.h>
+
+#include <glad/glad.h>
+
+//#include <SDL_ttf.h>
+#include "game.h"
+
+int main(int argc, char** argv) {
+    Game game;
+    bool success = game.Initialize();
+
+    if(success) {
+        game.RunLoop();
+    }
+
+    game.ShutDown();
+    return 0;
+}
